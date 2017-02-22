e5d4858a4faf122afc04f9639290407802971e88
Belén Ortega <belenortega@centraldereservas.com>
2017-02-22 16:43:46 +0100

    Primer commit

---
+++ _protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=base',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
